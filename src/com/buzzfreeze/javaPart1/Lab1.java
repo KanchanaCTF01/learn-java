10d5f328b3add88f34bd517866fb261ced5c128c
Kan <katookas.1990@gmail.com>
2021-04-16 16:15:53 +0700

    Lab1

---
+++ src/com/buzzfreeze/javaPart1/Lab1.java
@@ -0,0 +1,28 @@
+package com.buzzfreeze.javaPart1;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		int myInt = 20;
+		boolean myBoolean = true;
+		String myString = "Hi";
+		float myFloatNum = 5.69999996f;
+		char myLetter = '\u1233';		
+		
+		System.out.println("This is int " + myInt);
+		System.out.println("This is boolean " + myBoolean);
+		System.out.println("This is String " + myString);
+		System.out.println("This is float " + myFloatNum);
+		System.out.println("This is char " + myLetter);
+		
+		//System.out.println("This is String " + myString);
+		
+		/*
+		System.out.println("This is float " + myFloatNum);
+		System.out.println("This is char " + myLetter);
+		*/
+
+	}
+
+}
+
