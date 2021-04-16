f03a4e4c561baffffd30ed03ac8a452312fe416b
Kan <katookas.1990@gmail.com>
2021-04-16 16:19:13 +0700

    Lab1

--- src/com/buzzfreeze/javaPart1/Lab1.java
+++ src/com/buzzfreeze/javaPart1/Lab1.java
@@ -9,17 +9,17 @@ public class Lab1 {
 		float myFloatNum = 5.69999996f;
 		char myLetter = '\u1233';		
 		
-		System.out.println("This is int " + myInt);
-		System.out.println("This is boolean " + myBoolean);
-		System.out.println("This is String " + myString);
-		System.out.println("This is float " + myFloatNum);
-		System.out.println("This is char " + myLetter);
+		System.out.println("This is a int " + myInt);
+		System.out.println("This is a boolean " + myBoolean);
+		System.out.println("This is a String " + myString);
+		System.out.println("This is a float " + myFloatNum);
+		System.out.println("This is a char " + myLetter);
 		
-		//System.out.println("This is String " + myString);
+		//System.out.println("This is a String " + myString);
 		
 		/*
-		System.out.println("This is float " + myFloatNum);
-		System.out.println("This is char " + myLetter);
+		System.out.println("This is a float " + myFloatNum);
+		System.out.println("This is a char " + myLetter);
 		*/
 
 	}
