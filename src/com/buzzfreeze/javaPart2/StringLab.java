a4f47b0e1c13b1dc384afe4dfb6fd026f759af55
Kan <katookas.1990@gmail.com>
2021-04-17 15:23:58 +0700

    StringLab

---
+++ src/com/buzzfreeze/javaPart2/StringLab.java
@@ -0,0 +1,12 @@
+package com.buzzfreeze.javaPart2;
+
+public class StringLab {
+
+	public static void main(String[] args) {
+		char[] myChar = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'S', 't', 'r', 'i', 'n', 'g', ' ', 'f', 'r', 'o', 'm', ' ', 'C', 'h', 'a', 'r', '.'};
+		String myWord = new String(myChar);
+		System.out.println(myWord);
+
+	}
+
+}
