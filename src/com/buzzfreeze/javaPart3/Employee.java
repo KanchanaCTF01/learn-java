d44060e4385134489638b44b1b81265a6c28ae2e
Kan <katookas.1990@gmail.com>
2021-04-17 20:10:07 +0700

    Section13.2

---
+++ src/com/buzzfreeze/javaPart3/Employee.java
@@ -0,0 +1,34 @@
+package com.buzzfreeze.javaPart3;
+
+public class Employee {
+
+	public String firstname;
+	public String lastname;
+	private int salary;
+	public String position;
+	
+	public static void main(String[] args) {
+		Employee em1 = new Employee("Kanchana", "Saengdaeng","CEO",30000);
+		em1.checkPosition();
+	}
+
+	public Employee(String firstnameInput, String lastnameInput, String positionInput, int salaryInput) {
+		firstname = firstnameInput;
+		lastname = lastnameInput;
+		position = positionInput;
+		salary = salaryInput;
+	}
+
+	public void hello() {
+		System.out.println("Hello " + firstname);
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+	
+	public void checkPosition() {
+		System.out.println("My position is "+ position);
+	}
+
+}
